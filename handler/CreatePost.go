157659b6af4260640388383f376ad2a9faab9666
iwashi623 <wonderland.7878@gmail.com>
2023-02-11 20:36:19 +0900

    post_createのハンドラ実装

--- handler/CreatePost.go
+++ handler/CreatePost.go
@@ -1,8 +1,54 @@
 package handler
 
-import "github.com/iwashi623/go_posts/store"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/iwashi623/go_posts/entity"
+	"github.com/iwashi623/go_posts/store"
+)
 
 type CreatePost struct {
 	PostStore *store.PostStore
 	Validator *validator.Validate
+}
+
+func (cp *CreatePost) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var b struct {
+		Title string `json:"title" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+	}
+
+	err := cp.Validator.Struct(b)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+	}
+
+	p := &entity.Post{
+		Title:     b.Title,
+		Status:    entity.PostStatusDraft,
+		CreatedAt: time.Now(),
+	}
+	id, err := cp.PostStore.Create(p)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+	}
+
+	res := struct {
+		ID entity.PostID `json:"id"`
+	}{
+		ID: id,
+	}
+	RespondJSON(ctx, w, res, http.StatusOK)
 }
