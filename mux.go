5fbbe9c30b9d6dfd5af75132959e26375c940c41
iwashi623 <wonderland.7878@gmail.com>
2023-02-12 00:27:02 +0900

    muxのリファクタリング

--- mux.go
+++ mux.go
@@ -1,14 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/iwashi623/go_posts/handler"
+	"github.com/iwashi623/go_posts/store"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-		w.Write([]byte(`{"status": "ok"}`))
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
 
+	v := validator.New()
+	cp := &handler.CreatePost{Store: store.Posts, Validator: v}
+	mux.Post("/posts", cp.ServeHTTP)
+	lp := &handler.ListPost{Store: store.Posts}
+	mux.Get("/posts", lp.ServeHTTP)
 	return mux
 }
